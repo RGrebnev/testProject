26b9e7e0190ab0fe5328f0ecb177b4ce9dd99ce8
GrRoman <the.rg.inc@gmail.com>
2020-04-02 18:53:36 +0300

    change in kraken

--- src/Example.java
+++ src/Example.java
@@ -5,6 +5,6 @@ public class Example {
         System.out.println("OK");
         System.out.println("OK");
         System.out.println("OK");
-        System.out.println("OK");
+        System.out.println("notOK");
     }
 }
